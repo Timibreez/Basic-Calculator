d8fabb580dbb378808b84702980cceb48ecf8882
Timibreez <ffftimi@gmail.com>
2022-09-15 12:15:59 +0100

    initial commit

---
+++ TodoDot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoDot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+             
+            Console.Write("Whats your name ? ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadLine();
+        }
+    }
+}
